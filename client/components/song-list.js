982e00c4ad5db6cb436ed8a37d52d999311bd322
John Cheng <chengj@gmail.com>
2017-05-01 15:13:21 -0700

    Add song list component

---
+++ client/components/song-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongList = ({ songs }) => {
+  return (
+    <ul>
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ul>
+  );
+};
+
+module.exports = SongList;
--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SongList from './components/song-list';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello world!
+        <SongList />
       </div>
     );
   }
