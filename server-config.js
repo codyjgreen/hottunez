6364de9045d8abbf1b7113f32e8418e56ba78b24
John Cheng <chengj@gmail.com>
2017-05-01 12:20:51 -0700

    Add routes

--- server-config.js
+++ server-config.js
@@ -8,5 +8,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', handler.renderIndex);
+app.get('/api/songs', handler.fetchSongs);
+app.post('/api/songs', handler.createSong);
+app.get('/api/songs/:id', handler.fetchSong);
+app.put('/api/songs/:id', handler.updateSong);
+app.delete('/api/songs/:id', handler.deleteSong);
+app.get('/*', handler.render404);
 
 module.exports = app;
