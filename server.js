9dbb1a3c4aaa3d1cd4ad852fa00b040338388e6a
John Cheng <chengj@gmail.com>
2017-06-27 16:03:23 -0700

    Add heroku postbuild script and port env variable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('./server-config');
-var port = 8080;
+var port = process.env.PORT || 8080;
 
-app.listen(8080);
-console.log('Server now listening on port ' + port);
+app.listen(port, function() {
+  console.log('Server now listening on port ' + port);
+});
